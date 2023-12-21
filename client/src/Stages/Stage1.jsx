da9ad3bdeb17e0e233c7176c3400c9f73923baa2
Sijan Bhattarai <sijanbhattarai@Sijans-MacBook.local>
2023-12-21 10:32:23 -0500

    reverted stage1.jsx

--- client/src/Stages/Stage1.jsx
+++ client/src/Stages/Stage1.jsx
@@ -1,15 +1,5 @@
 import React from "react";
-import { Button } from "../components/Button";
 
 export function Stage1() {
-  return (
-    <div>
-      <h2>Risk Tolerance Game</h2>
-      <p>Choose an option:</p>
-      <div className="flex justify-center">
-          <Button className="m-5">Option AA</Button>
-          <Button className="m-5">Option B</Button>
-        </div>
-    </div>
-  );
-} 
+  return <div>"Stage1" component has not yet implemented</div>;
+}
