58d618f0217d9431ffac3e3de9ae6dbbfb2a6c61
Sijan Bhattarai <sijanbhattarai@Sijans-MacBook.local>
2023-12-26 11:03:29 -0500

    iframe added

--- client/src/Stages/Stage1.jsx
+++ client/src/Stages/Stage1.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export function Stage1() {
-  return <div>"Stage1" component not yet implemented</div>;
-}
+  return (
+    <div class="experiment-container">
+        <div class="qualtrics-survey">
+            <iframe id="qualtrics-iframe" src="" width="100%" height="600px" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+  );
+} 
